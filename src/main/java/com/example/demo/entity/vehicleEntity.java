f93cc95f910b74d9645b22ddf1292430777a89bf
bala-amypo <bala@amypo.in>
2025-12-16 16:30:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/vehicleEntity.java
+++ src/main/java/com/example/demo/entity/vehicleEntity.java
@@ -6,5 +6,22 @@ import jakarta.persistence.*;
 public class vehicleEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(Long id){
+        this.id = id;
+    }
 }
